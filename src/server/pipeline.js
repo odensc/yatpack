c9a07f4735fdd0ba593aa4239011d283aab69461
Oden <hello@odensc.me>
2020-07-30 20:49:53 -0700

    Update pipeline.js

--- src/server/pipeline.js
+++ src/server/pipeline.js
@@ -8,5 +8,6 @@ v4l2src device=/dev/video0 ! video/x-raw, width=1920, height=1080, framerate=30/
   )} control-rate=1 iframeinterval=-1 preset-level=3 profile=0 insert-sps-pps=1 SliceIntraRefreshInterval=60 ! video/x-h265, stream-format=byte-stream ! \
   mpegtsmux alignment=7 name=mux ! srtserversink name=srt uri="srt://${
 		process.env.SRT_IP
-  }:1935?streamid=input/live/cam" latency=300
+  }:1935?streamid=input/live/cam" latency=300  \	
+  alsasrc device=hw:2 ! audioconvert ! opusenc audio-type=voice ! mux.
 `);
